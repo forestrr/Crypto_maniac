6d4c6be26184421a967037c33c44ddb4ce2a7be5
forestrr <afimcy78@gmail.com>
2022-01-29 01:57:22 +0400

    the cyptomaniac app has been desinged to the basic part the coinpage router , home page router has been made , the component like banner , the carousel and the header section has been made . contextapi and routerdom is also used and the alice react carousel has been made and the axios to fetch data from the coingecko api and material ui has been used to make many components and styling has been done

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,59 @@
+import { Container, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import Carousel from './Carousel';
+
+const useStyles=makeStyles(()=>({
+    banner:{
+        backgroundImage:"url(./banner2.jpg)",
+    },
+    bannercontent:{
+        height:400,
+        display:"flex",
+        flexDirection:"column",
+        paddingTop:25,
+        JustifyContent:"space-around"
+        
+        
+    },tagline:{
+        display: "flex",
+        height: "40%",
+        flexDirection:"column",
+        justifyContent:"center",
+        textAlign:'center',
+    }
+    
+}))
+
+const Banner = () => {
+    const classes=useStyles()
+    
+          return  <div className={classes.banner} >
+              <Container className={classes.bannercontent} >
+        <div className={classes.tagline}>
+            <Typography 
+            variant="h2"
+            style={{
+                color:"gold",
+                fontWeight:"bold",
+                marginBottom:15,
+                fontFamily:"Montserrat"
+            }}>
+                CryptoManiac.
+            </Typography>
+            <Typography
+            variant="subtitle2"
+            style={{
+                color:"darkgrey",
+                textTransform:"capitalize",
+                fontFamily:"Montserrat"
+            }}>
+                Get All information about your favourite Crypto 
+            </Typography>
+
+        </div>   
+        <Carousel/>
+      </Container>
+  </div>
+};
+
+export default Banner;
---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
+
+const useStyle=makeStyles(()=> ({
+    title:{
+        flex:1,
+        color: "white",
+        fontFamily:"Montserrat",
+        fontWeight:"bold",
+        cursor: "pointer",
+
+    }
+}))
+const Header = () => {
+    const classes =useStyle();
+    const history=useHistory();
+    const {currency,setCurrency}=CryptoState();
+    console.log(currency)
+    const darkTheme=createTheme({
+        palette:{
+            primary:{
+                main:'#fff'
+            },
+            type:"dark",
+        }
+    })
+  return <div>
+    <ThemeProvider theme={darkTheme}>
+
+     <AppBar color='transparent' position='static' >
+         <Container>
+             <Toolbar>
+                 <Typography
+                 onClick={()=>history.push("/")} 
+                 className={classes.title} 
+                 variant="h6">
+                     CryptoManiac.</Typography>
+                 <Select 
+                 variant='outlined'
+                 style={{
+                     width: 100,
+                     height:40,
+                     marginRight:15,
+                    }} 
+                    value={currency}
+                    onChange={(e)=>setCurrency(e.target.value)}
+                    >
+                     <MenuItem value={"USD"} >USD</MenuItem>
+                     <MenuItem value={"INR"} >INR</MenuItem>
+                 </Select>
+             </Toolbar>
+         </Container>
+
+     </AppBar>
+     </ThemeProvider>
+  </div>;
+};
+
+export default Header;
